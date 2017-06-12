094463716b68388ac6c711174fe408cf0f5a291e
Wizardnet972 <wizardnet972@gmail.com>
2017-06-12 17:13:37 +0300

    NativeScript Modules

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -1,8 +1,61 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+
+import { GroceryListService } from "../../shared/grocery/grocery-list.service";
+import { Grocery } from "../../shared/grocery/grocery";
+import { TextField } from "ui/text-field";
 
 @Component({
   selector: "list",
   templateUrl: "pages/list/list.html",
-  styleUrls: ["pages/list/list-common.css", "pages/list/list.css"]
+  styleUrls: ["pages/list/list-common.css", "pages/list/list.css"],
+  providers: [GroceryListService]
 })
-export class ListComponent {}
+export class ListComponent implements OnInit {
+
+  groceryList: Array<Grocery> = [];
+  grocery = "";
+  @ViewChild("groceryTextField") groceryTextField: ElementRef;
+  isLoading = false;
+  listLoaded = false;
+
+  constructor(private groceryListService: GroceryListService) {
+  }
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.groceryListService.load()
+      .subscribe(loadedGroceries => {
+        loadedGroceries.forEach((groceryObject) => {
+          this.groceryList.unshift(groceryObject);
+        });
+        this.isLoading = false;
+        this.listLoaded = true;
+      });
+  }
+
+  add() {
+    if (this.grocery.trim() === "") {
+      alert("Enter a grocery item");
+      return;
+    }
+
+    // Dismiss the keyboard
+    let textField = <TextField>this.groceryTextField.nativeElement;
+    textField.dismissSoftInput();
+
+    this.groceryListService.add(this.grocery)
+      .subscribe(
+      groceryObject => {
+        this.groceryList.unshift(groceryObject);
+        this.grocery = "";
+      },
+      () => {
+        alert({
+          message: "An error occurred while adding an item to your list.",
+          okButtonText: "OK"
+        });
+        this.grocery = "";
+      }
+      )
+  }
+}
--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -1,8 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
 import { User } from "../../shared/user/user";
 import { UserService } from "../../shared/user/user.service";
 import { Router } from "@angular/router";
+import { Page } from "ui/page";
+import { Color } from "color";
+import { View } from "ui/core/view";
 
 @Component({
     selector: "my-app",
@@ -10,15 +13,22 @@ import { Router } from "@angular/router";
     templateUrl: "pages/login/login.html",
     styleUrls: ["pages/login/login-common.css", "pages/login/login.css"],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
     user: User;
     isLoggingIn = true;
+    @ViewChild("container") container: ElementRef;
 
-    constructor(private router: Router, private userService: UserService) {
+    constructor(private router: Router, private userService: UserService, private page: Page) {
         this.user = new User();
         this.user.email = "user@nativescript.org";
         this.user.password = "password";
     }
+
+    ngOnInit() {
+        this.page.actionBarHidden = true;
+        this.page.backgroundImage = "res://bg_login";
+    }
+
     submit() {
         if (this.isLoggingIn) {
             this.login();
@@ -26,6 +36,7 @@ export class LoginComponent {
             this.signUp();
         }
     }
+
     login() {
         this.userService.login(this.user)
             .subscribe(
@@ -43,7 +54,13 @@ export class LoginComponent {
             () => alert("Unfortunately we were unable to create your account.")
             );
     }
+
     toggleDisplay() {
         this.isLoggingIn = !this.isLoggingIn;
+        let container = <View>this.container.nativeElement;
+        container.animate({
+            backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
+            duration: 200
+        });
     }
 }
